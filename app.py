4741b8ce5eef4bb54b323da6af0b00a7f43b89b5
ABDUL RAHMAN <72724072+Rahman-Lone@users.noreply.github.com>
2026-05-05 12:12:09 +0800

    Fix model URL to use download link

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ IMG_SIZE = (299, 299)
 @st.cache_resource
 def load_model():
     # PASTE YOUR COPIED GITHUB RELEASE LINK HERE:
-    MODEL_URL = "https://github.com/Rahman-Lone/rabbit-app/releases/tag/v1.1/rabbit_breed_final_model.keras"
+    MODEL_URL = "https://github.com/Rahman-Lone/rabbit-app/releases/download/v1.1/rabbit_breed_final_model.keras"
   
     # If the model isn't already downloaded, download it from the release
     if not os.path.exists(MODEL_PATH):
